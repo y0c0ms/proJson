31acf71261a42beb6dfbfb2f6694d0cd182c7351
AlexandreMendesISCTE <agmsa3@iscte-iul.pt>
2026-04-27 00:23:32 +0100

    add JsonReferenceTest

---
+++ src/test/kotlin/projson/JsonReferenceTest.kt
@@ -0,0 +1,19 @@
+package projson
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class JsonReferenceTest {
+
+    @Test
+    fun `reference serializes as dollar-ref object`() {
+        val ref = JsonReference("9e2e6c64-3236-45b7-8b8a-11271c69e4df")
+        assertEquals("{\"\$ref\": \"9e2e6c64-3236-45b7-8b8a-11271c69e4df\"}", ref.toJsonString())
+    }
+
+    @Test
+    fun `toString delegates to toJsonString`() {
+        val ref = JsonReference("abc-123")
+        assertEquals("{\"\$ref\": \"abc-123\"}", ref.toString())
+    }
+}
